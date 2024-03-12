0545c97b4ecf4c4dd20ca921eac949f7e1d0e5bb
xiaole <2366092957@qq.com>
2024-03-12 14:42:04 +0800

    修改默认配置

--- project/one-vue3-app/vue.config.js
+++ project/one-vue3-app/vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,
+  devServer:{
+    port:61889, // 启动端口号
+    open:false,  // 启动后是否自动打开网页
+  }
 })
